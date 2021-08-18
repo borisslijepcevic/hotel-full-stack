bfff276a8d289a2e987df2aa30f9da9ef68262bd
boris87 <boris.slijepcevic87@gmail.com>
2021-08-18 12:14:18 +0200

    added my project

---
+++ src/main/java/com/boris/hotelmanagementfullstack/HotelManagementFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.boris.hotelmanagementfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementFullStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boris/hotelmanagementfullstack/HotelManagementFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boris.hotelmanagementfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
